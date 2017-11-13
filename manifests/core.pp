0135c2986d1fb2198f40ef0b2c1a62241e255e5a
Ondrej Sika <ondrej@ondrejsika.com>
2017-11-13 20:15:43 +0100

    manifests/core - use ssh_authorized_key instead of file_line  #1

--- manifests/core.pp
+++ manifests/core.pp
@@ -36,20 +36,12 @@ class dotfiles::core {
     content => template('dotfiles/core/c'),
   }
 
-  file { ['/root/.ssh', '/home/sika/.ssh']:
-    ensure  => 'directory',
-  }->
-  file { ['/root/.ssh/authorized_keys', '/home/sika/.ssh/authorized_keys']:
-    ensure  => 'present',
-    mode    => '600',
-  }->
-  file_line {'ssh_authorized_key_sika':
-    path => '/home/sika/.ssh/authorized_keys',
-    line => template('dotfiles/core/id_rsa.pub'),
-  }->
-  file_line {'ssh_authorized_key_root':
-    path => '/root/.ssh/authorized_keys',
-    line => template('dotfiles/core/id_rsa.pub'),
+  ssh_authorized_key { 'ondrej@ondrejsika.com':
+    ensure => present,
+    user   => ['root', 'sika'],
+    type   => 'ssh-rsa',
+    #key    => template('dotfiles/core/ssh_key_pub'),
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCslNKgLyoOrGDerz9pA4a4Mc+EquVzX52AkJZz+ecFCYZ4XQjcg2BK1P9xYfWzzl33fHow6pV/C6QC3Fgjw7txUeH7iQ5FjRVIlxiltfYJH4RvvtXcjqjk8uVDhEcw7bINVKVIS856Qn9jPwnHIhJtRJe9emE7YsJRmNSOtggYk/MaV2Ayx+9mcYnA/9SBy45FPHjMlxntoOkKqBThWE7Tjym44UNf44G8fd+kmNYzGw9T5IKpH1E1wMR+32QJBobX6d7k39jJe8lgHdsUYMbeJOFPKgbWlnx9VbkZh+seMSjhroTgniHjUl8wBFgw0YnhJ/90MgJJL4BToxu9PVnH',
   }
 
   $install_packages = [
