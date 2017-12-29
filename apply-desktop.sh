a45f039c3f47197382da0c08f084adbce3434f75
Ondrej Sika <ondrej@ondrejsika.com>
2017-12-29 21:20:56 +0100

    apply-* - add module path for gem installed puppet

--- apply-desktop.sh
+++ apply-desktop.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-puppet apply --modulepath '/etc/puppetlabs/code/environments/production/modules:..' -e 'class {"dotfiles::install::desktop": }' --hiera_config hiera.yaml
+puppet apply \
+    --modulepath '/etc/puppetlabs/code/modules:/etc/puppetlabs/code/environments/production/modules:..' \
+    -e 'class {"dotfiles::install::desktop": }' \
+    --hiera_config hiera.yaml
+
--- apply-server.sh
+++ apply-server.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-puppet apply --modulepath '/etc/puppetlabs/code/environments/production/modules:..' -e 'class {"dotfiles::install::server": }' --hiera_config hiera.yaml
+puppet apply \
+    --modulepath '/etc/puppetlabs/code/modules:/etc/puppetlabs/code/environments/production/modules:..' \
+    -e 'class {"dotfiles::install::server": }' \
+    --hiera_config hiera.yaml
+
