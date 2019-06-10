4f322fadf5ad26e70fcf142f406854738d76f8ad
Ondrej Sika <ondrej@ondrejsika.com>
2019-06-10 13:18:00 +0200

    apply-server.sh - rewrite to shell form (remove Puppet)

--- apply-server.sh
+++ apply-server.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-puppet apply \
-    --modulepath '/etc/puppetlabs/code/modules:/etc/puppetlabs/code/environments/production/modules:..' \
-    -e 'class {"dotfiles::install::server": }' \
-    --hiera_config hiera.yaml
+# Bash
+SYSTEM_BASHRC=/etc/bash.bashrc
+echo "# Created by ondrejsika/dotfiles" > $SYSTEM_BASHRC
+echo ". $(pwd)/core/3rdparty/git-prompt.sh" >> $SYSTEM_BASHRC
+echo ". $(pwd)/core/3rdparty/git-completion.sh" >> $SYSTEM_BASHRC
+echo ". $(pwd)/core/bashrc" >> $SYSTEM_BASHRC
 
+# VIM
+rm -rf /etc/vim
+mkdir -p /etc/vim
+ln -s $(pwd)/core/vimrc /etc/vim/vimrc
