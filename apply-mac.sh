b878b52140a64e5df3e5f3f132a4165906df7aec
Ondrej Sika <ondrej@ondrejsika.com>
2019-06-10 13:11:31 +0200

    vimrc - rewrite vimrc for Mac

--- apply-mac.sh
+++ apply-mac.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-rm -rf ~/.hammerspoon
-ln -s $(pwd)/mac/hammerspoon ~/.hammerspoon 
-
+# Bash
 echo ". $(pwd)/core/3rdparty/git-prompt.sh" >> ~/.profile
 echo ". $(pwd)/core/3rdparty/git-completion.sh" >> ~/.profile
 echo ". $(pwd)/core/bashrc" >> ~/.profile
@@ -12,3 +10,11 @@ echo ". ~/.bashrc" >> ~/.profile
 
 echo "" >> ~/.bash_aliases
 echo ". ~/.bash_aliases" >> ~/.profile
+
+# VIM
+rm -rf ~/.vimrc
+ln -s $(pwd)/core/vimrc ~/.vimrc
+
+# Hammer Spoon
+rm -rf ~/.hammerspoon
+ln -s $(pwd)/mac/hammerspoon ~/.hammerspoon
