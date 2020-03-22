e0b0ae8e6510260840b352ccd279ca456078f8c5
Ondrej Sika <ondrej@ondrejsika.com>
2020-03-22 16:14:34 +0100

    apply-server.sh - add complete_alias to bashrc

--- apply-server.sh
+++ apply-server.sh
@@ -5,6 +5,7 @@ SYSTEM_BASHRC=/etc/bash.bashrc
 echo "# Created by ondrejsika/dotfiles" > $SYSTEM_BASHRC
 echo ". $(pwd)/core/3rdparty/git-prompt.sh" >> $SYSTEM_BASHRC
 echo ". $(pwd)/core/3rdparty/git-completion.sh" >> $SYSTEM_BASHRC
+echo ". $(pwd)/core/3rdparty/complete_alias" >> $SYSTEM_BASHRC
 echo ". $(pwd)/core/bashrc" >> $SYSTEM_BASHRC
 
 # VIM
